700624f3f27d2801564b1bd0317690f0bf35561a
Savage-Steve <97632667+Savage-Steve@users.noreply.github.com>
2024-04-01 00:26:53 +0000

    Update

--- script.js
+++ script.js
@@ -7,73 +7,79 @@ window.addEventListener('DOMContentLoaded', () => {
     color: '#1d1d1d',
   };
 
+  var clicked = 0;
+
   var manager = nipplejs.create(options);
   var cursorObj = document.getElementById('cursorObj');
-  var mouseButton = document.getElementById('mouseButton');
+var highlightBox = document.getElementById('highlight-box')
+  var mouseButton = document.getElementById('mouseObj');
   var clickableElements = document.getElementsByClassName('clickable');
-  
-  var isButtonClicked = false;
-  
-  
 
-function isClickable() {
-  for (var i = 0; i < clickableElements.length; i++) {
-    var elementRect = clickableElements[i].getBoundingClientRect();
-    var cursorObjRect = cursorObj.getBoundingClientRect();
+  function isClickable() {
+    for (var i = 0; i < clickableElements.length; i++) {
+      var elementRect = clickableElements[i].getBoundingClientRect();
+      var cursorObjRect = cursorObj.getBoundingClientRect();
 
-    if (
-      cursorObjRect.left < elementRect.right &&
-      cursorObjRect.right > elementRect.left &&
-      cursorObjRect.top < elementRect.bottom &&
-      cursorObjRect.bottom > elementRect.top
-    ) {
-      return true;
+      if (
+        cursorObjRect.left < elementRect.right &&
+        cursorObjRect.right > elementRect.left &&
+        cursorObjRect.top < elementRect.bottom &&
+        cursorObjRect.bottom > elementRect.top
+      ) {
+        return true;
+      }
     }
-  }
 
-  return false;
-}
-  
-  var isClicked = false;
+    return false;
+  }
+  var isButtonHeld = false;
   function handleButtonClick() {
-    isClicked = true;
-    wait(1000).then(() => {
-      isClicked = false;
-    })
+    clicked++;
+    if (clicked === 1){
+      console.log("Mouse Down")
+    }
+    if (clicked == 2) {
+      isButtonHeld = true;
+      console.log('Holding Mouse');
+    }
+    if (clicked >= 3) {
+      isButtonHeld = false;
+      clicked = 0;
+      console.log('Mouse Up');
+    }
   }
   mouseButton.addEventListener('click', handleButtonClick);
 
+
   manager.on('move', (evt, data) => {
-  var angle = data.angle.radian;
-  var magnitude = data.distance / 5;
+    var angle = data.angle.radian;
+    var magnitude = data.distance / 25;
 
-  var xDisplacement = magnitude * Math.cos(angle);
-  var yDisplacement = magnitude * Math.sin(angle);
-    
-  
-  var currentLeft = parseFloat(cursorObj.style.left) || 0;
-  var currentTop = parseFloat(cursorObj.style.top) || 0;
+    var xDisplacement = magnitude * Math.cos(angle);
+    var yDisplacement = magnitude * Math.sin(angle);
 
-  var newLeft = currentLeft + xDisplacement;
-  var newTop = currentTop - yDisplacement;
 
-  var screenWidth = window.screen.width - cursorObj.offsetWidth;
-  var screenOffsetY = window.screen.height - window.innerHeight;
-  var screenHeight = window.screen.height - cursorObj.offsetHeight - screenOffsetY;
+    var currentLeft = parseFloat(cursorObj.style.left) || 0;
+    var currentTop = parseFloat(cursorObj.style.top) || 0;
 
-  if (newLeft >= 0 && newLeft <= screenWidth) {
-    cursorObj.style.left = newLeft + 'px';
-  }
-  
-  if (newTop >= 0 && newTop <= screenHeight) {
-    cursorObj.style.top = newTop + 'px';
-  }
-  if (isClickable() && isClicked) {
-    console.log('cursorObj is hovering over an element with the specified class');
-  } else {
-    console.log('object not clickable!);
-  }
-});
+    var newLeft = currentLeft + xDisplacement;
+    var newTop = currentTop - yDisplacement;
+
+    var screenWidth = window.screen.width - cursorObj.offsetWidth;
+    var screenOffsetY = window.screen.height - window.innerHeight;
+    var screenHeight = window.screen.height - cursorObj.offsetHeight - screenOffsetY;
+
+    if (newLeft >= 0 && newLeft <= screenWidth) {
+      cursorObj.style.left = newLeft + 'px';
+    }
+
+    if (newTop >= 0 && newTop <= screenHeight) {
+      cursorObj.style.top = newTop + 'px';
+    }
+    if  (isButtonHeld = true) {
+highlightBox.size = highlightBox.size + 5;
+    }
+  });
   var joystickBack = document.querySelector('#joystick-container').querySelector('#nipple_0_0').querySelector('.back');;
   var joystickFront = document.querySelector('#joystick-container').querySelector('#nipple_0_0').querySelector('.front')
 
